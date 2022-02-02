2b4145c213c7c93965a85034780aab7c869974bd
Iván Piccardo <piccardo.ivan@gmail.com>
2022-02-02 01:28:15 -0300

    Final test

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -14,17 +14,15 @@ export const GifGrid = ({ category }) => {
 
   return (
     <>
-      <h3>{category}</h3>
+      <h3 className="animate__animated animate__fadeIn">{category}</h3>
 
-      {loading && <p>Loading</p>}
+      {loading && <p className="animate__animated animate__flash">Loading</p>}
 
-      {
-        <div className="card-grid">
-          {images.map((img) => {
-            return <GifGridItem key={img.id} {...img} />;
-          })}
-        </div>
-      }
+      <div className="card-grid">
+        {images.map((img) => (
+          <GifGridItem key={img.id} {...img} />
+        ))}
+      </div>
     </>
   );
 };
